40e428a94bebdf48286b905d332b47642440073f
Ana Christina <anachristinaac@gmail.com>
2023-07-03 16:06:42 +0100

    Added example of cartesian control using transformInterpolations method.

--- examples/cartesian.py
+++ examples/cartesian.py
@@ -47,7 +47,7 @@ def main():
             # The getPosition method returns a vector containing the pose
             # (x, y, z, wx, wy, wz) of any joint, chain, or sensor in meters and
             # radians, relative to the chosen frame. The last parameter sets if
-            # the values will be used to determine the pose.
+            # the sensors will be used to determine the pose.
             initial_pose = motion_proxy.getPosition(arm,
                                                     motion.FRAME_ROBOT, False)
 
@@ -95,6 +95,66 @@ def main():
                                             targets,  # target vector
                                             or_wx + or_wy + or_wz,  # what to control
                                             [3, 6, 9, 12])  # relative times in seconds corresponding to the path points
+
+    #  ======== USING ANIMATION METHOD - transformInterpolations ===============
+    if method == 2:
+        # ================= Making the arms move ===============================
+        arms = ["LArm", "RArm"]
+        for arm in arms:
+            # The getTransform method returns an homogeneous transformation
+            # matrix (HTM) to any joint, chain, or sensor relative to the chosen
+            # frame. The last parameter sets if the sensors will be used to
+            # determine the transformation.
+            initial_transform = motion_proxy.getTransform(arm,
+                                                          motion.FRAME_ROBOT,
+                                                          False)
+
+            pos_x = 1
+            pos_y = 2
+            pos_z = 4
+            or_wx = 8
+            or_wy = 16
+            or_wz = 32
+
+            # Defining the HTM with different translation only:
+            if arm == "LArm":
+                target = [initial_transform[0], initial_transform[1], initial_transform[2], 0.06,
+                          initial_transform[4], initial_transform[5], initial_transform[6], 0.05,
+                          initial_transform[8], initial_transform[9], initial_transform[10], 0.3,
+                          0, 0, 0, 1]
+            else:
+                target = [initial_transform[0], initial_transform[1], initial_transform[2], 0.06,
+                          initial_transform[4], initial_transform[5], initial_transform[6], -0.05,
+                          initial_transform[8], initial_transform[9], initial_transform[10], 0.3,
+                          0, 0, 0, 1]
+
+            motion_proxy.transformInterpolations(arm,  # effector
+                                                 motion.FRAME_ROBOT,  # frame
+                                                 [target, initial_transform],  # target vector
+                                                 pos_x + pos_y + pos_z,  # what to control
+                                                 [3, 6])  # relative times in seconds corresponding to the path points
+
+        # ================= Making the head move ===============================
+        initial_transform = motion_proxy.getTransform("Head",
+                                                      motion.FRAME_ROBOT,
+                                                      False)
+
+        # Defining the HTMs with different rotations only:
+        angle = 30 * math.pi / 180
+        target1 = [math.cos(angle), -math.sin(angle), 0, initial_transform[3],
+                   math.sin(angle), math.cos(angle), 0, initial_transform[7],
+                   0, 0, 1, initial_transform[11],
+                   0, 0, 0, 1]
+        target2 = [math.cos(-angle), -math.sin(-angle), 0, initial_transform[3],
+                   math.sin(-angle), math.cos(-angle), 0, initial_transform[7],
+                   0, 0, 1, initial_transform[11],
+                   0, 0, 0, 1]
+        target = [target1, initial_transform, target2, initial_transform]
+        motion_proxy.transformInterpolations("Head",  # effector
+                                             motion.FRAME_ROBOT,  # frame
+                                             target, # target vector
+                                             or_wx + or_wy + or_wz,  # what to control
+                                             [3, 6, 9, 12])  # relative times in seconds corresponding to the path points
 
     # The rest() method sends the robot to a relaxed and safe position and sets
     # its motors off. For NAO H25, if the robot is standing, it goes to the
