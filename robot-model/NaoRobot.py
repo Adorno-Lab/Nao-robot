f8c3245c47c2d79c32cecb746f375652340abd5c
j61327ac <anachristinaac@gmail.com>
2024-04-24 14:40:03 +0100

    Updating robot model.

--- robot-model/NaoRobot.py
+++ robot-model/NaoRobot.py
@@ -5,14 +5,14 @@ from dqrobotics.robot_modeling import DQ_SerialManipulatorDH
 
 class NaoRobot:
     def __init__(self):
-        # Robot length parameters.
+        # Robot length parameters
         self.neck_offset_z = 0.1265
         self.bottom_camera_x = 0.05071
         self.bottom_camera_z = 0.01774
         self.top_camera_x = 0.05871
         self.top_camera_z = 0.06364
-        self.top_camera_angle_y = 0.0209
-        self.bottom_camera_angle_y = 0.6929
+        self.top_camera_wy = 0.0209
+        self.bottom_camera_wy = 0.6929
 
         self.shoulder_offset_z = 0.100
         self.shoulder_offset_y = 0.098
@@ -28,8 +28,8 @@ class NaoRobot:
         self.tibia_length = 0.1029
         self.foot_height = 0.04519
 
-        # Parameters to indicate if joint is rotational or prismatic,
-        # to be used with DQ_SerialManipulatorDH.
+        # Parameters to indicate if joint is rotational or prismatic
+        # (to be used with DQ_SerialManipulatorDH)
         self.dq_joint_rotational = 0
         self.dq_joint_prismatic = 1
 
@@ -39,12 +39,22 @@ class TopCamera(NaoRobot):
     Class for the serial head chain, from torso to top camera.
     Joints: HeadYaw, HeadPitch
     """
-    def kinematics(self):
+    def get_joints_names(self) -> [str]:
+        """
+        Return the name of the joints in the chain.
+
+        :return: a list with the name of the joints in the chain
+        """
+        return ["HeadYaw", "HeadPitch"]
+
+    def kinematics(self) -> DQ_SerialManipulatorDH:
         """
         Create the DQ_kinematics object for the chain.
 
-        :return: DQ_SerialManipulatorDH object.
+        :return: DQ_SerialManipulatorDH object
         """
+
+        # Denavit-Hartenberg parameters
         DH_theta = [0, 0]
         DH_d = [0, 0]
         DH_a = [0, 0]
@@ -54,15 +64,15 @@ class TopCamera(NaoRobot):
 
         obj = DQ_SerialManipulatorDH(DH_matrix)
 
-        # Setting base and reference:
+        # Setting base and reference to the torso
         tz = 1 + DQ.E * (1/2) * DQ([0, 0, self.neck_offset_z])
         obj.set_base_frame(tz)
         obj.set_reference_frame(tz)
 
-        # Setting the end effector:
+        # Setting end effector
         rx = cos((pi/2)/2) + DQ.i * sin((pi/2)/2)
         txz = 1 + DQ.E * (1/2) * DQ([self.top_camera_x, 0, self.top_camera_z])
-        ry = cos(self.top_camera_angle_y/2) + DQ.j * sin(self.top_camera_angle_y/2)
+        ry = cos(self.top_camera_wy/2) + DQ.j * sin(self.top_camera_wy/2)
         obj.set_effector(rx * txz * ry)
 
         return obj
@@ -73,12 +83,22 @@ class BottomCamera(NaoRobot):
     Class for the serial head chain, from torso to bottom camera.
     Joints: HeadYaw, HeadPitch
     """
-    def kinematics(self):
+    def get_joints_names(self) -> [str]:
+        """
+        Return the name of the joints in the chain.
+
+        :return: a list with the name of the joints in the chain
+        """
+        return ["HeadYaw", "HeadPitch"]
+
+    def kinematics(self) -> DQ_SerialManipulatorDH:
         """
         Create the DQ_kinematics object for the chain.
 
-        :return: DQ_SerialManipulatorDH object.
+        :return: DQ_SerialManipulatorDH object
         """
+
+        # Denavit-Hartenberg parameters
         DH_theta = [0, 0]
         DH_d = [0, 0]
         DH_a = [0, 0]
@@ -88,15 +108,15 @@ class BottomCamera(NaoRobot):
 
         obj = DQ_SerialManipulatorDH(DH_matrix)
 
-        # Setting base and reference:
+        # Setting base and reference to the torso
         tz = 1 + DQ.E * (1/2) * DQ([0, 0, self.neck_offset_z])
         obj.set_base_frame(tz)
         obj.set_reference_frame(tz)
 
-        # Setting end effector:
+        # Setting end effector
         rx = cos((pi/2)/2) + DQ.i * sin((pi/2)/2)
         txz = 1 + DQ.E * (1/2) * DQ([self.bottom_camera_x, 0, self.bottom_camera_z])
-        ry = cos(self.bottom_camera_angle_y/2) + DQ.j * sin(self.bottom_camera_angle_y/2)
+        ry = cos(self.bottom_camera_wy/2) + DQ.j * sin(self.bottom_camera_wy/2)
         obj.set_effector(rx * txz * ry)
 
         return obj
@@ -105,16 +125,28 @@ class BottomCamera(NaoRobot):
 class LeftArm(NaoRobot):
     """
     Class for the serial left arm chain, from torso to left hand.
-    Joints: LShoulderPitch, LShoulderRoll, LElbowYaw, LElbowRow
+    Joints: LShoulderPitch, LShoulderRoll, LElbowYaw,
+            LElbowRoll, LWristYaw
     """
-    def kinematics(self):
+    def get_joints_names(self) -> [str]:
+        """
+        Return the name of the joints in the chain.
+
+        :return: a list with the name of the joints in the chain
+        """
+        return ["LShoulderPitch", "LShoulderRoll", "LElbowYaw",
+                "LElbowRoll", "LWristYaw"]
+
+    def kinematics(self) -> DQ_SerialManipulatorDH:
         """
         Create the DQ_kinematics object for the chain.
 
-        :return: DQ_SerialManipulatorDH object.
+        :return: DQ_SerialManipulatorDH object
         """
+
+        # Denavit-Hartenberg parameters
         DH_theta = [0, pi/2, 0, 0, 0]
-        DH_d = [self.shoulder_offset_y, 0, self.upper_arm_length, 0, 0]
+        DH_d = [0, 0, self.upper_arm_length, 0, self.lower_arm_length]
         DH_a = [0, self.elbow_offset_y, 0, 0, 0]
         DH_alpha = [pi/2, pi/2, -pi/2, pi/2, 0]
         DH_type = [self.dq_joint_rotational] * 5
@@ -122,16 +154,16 @@ class LeftArm(NaoRobot):
 
         obj = DQ_SerialManipulatorDH(DH_matrix)
 
-        # Setting base and reference:
-        tz = 1 + DQ.E * (1/2) * DQ([0, 0, self.shoulder_offset_z])
-        rx = cos((-pi / 2) / 2) + DQ.i * sin((-pi / 2) / 2)
-        obj.set_base_frame(tz * rx)
-        obj.set_reference_frame(tz * rx)
+        # Setting base and reference to the torso
+        tyz = 1 + DQ.E * (1/2) * DQ([0, self.shoulder_offset_y, self.shoulder_offset_z])
+        rx = cos((-pi/2)/2) + DQ.i * sin((-pi/2)/2)
+        obj.set_base_frame(tyz * rx)
+        obj.set_reference_frame(tyz * rx)
 
-        # Setting end effector:
+        # Setting end effector
         rx = cos((-pi/2)/2) + DQ.i * sin((-pi/2)/2)
         rz = cos((-pi/2)/2) + DQ.k * sin((-pi/2)/2)
-        txz = 1 + DQ.E * (1/2) * DQ([self.hand_offset_x + self.lower_arm_length, 0, -self.hand_offset_z])
+        txz = 1 + DQ.E * (1/2) * DQ([self.hand_offset_x, 0, -self.hand_offset_z])
         obj.set_effector(rx * rz * txz)
 
         return obj
@@ -140,16 +172,27 @@ class LeftArm(NaoRobot):
 class RightArm(NaoRobot):
     """
     Class for the serial right arm chain, from torso to right hand.
-    Joints: RShoulderPitch, RShoulderRoll, RElbowYaw, RElbowRow
+    Joints: RShoulderPitch, RShoulderRoll, RElbowYaw, RElbowRoll, RWristYaw
     """
-    def kinematics(self):
+    def get_joints_names(self) -> [str]:
+        """
+        Return the name of the joints in the chain.
+
+        :return: a list with the name of the joints in the chain
+        """
+        return ["RShoulderPitch", "RShoulderRoll", "RElbowYaw",
+                "RElbowRoll", "LWristYaw"]
+
+    def kinematics(self) -> DQ_SerialManipulatorDH:
         """
         Create the DQ_kinematics object for the chain.
 
         :return: DQ_SerialManipulatorDH object.
         """
+
+        # Denavit-Hartenberg parameters
         DH_theta = [0, pi/2, 0, 0, 0]
-        DH_d = [-self.shoulder_offset_y, 0, self.upper_arm_length, 0, 0]
+        DH_d = [0, 0, self.upper_arm_length, 0, self.lower_arm_length]
         DH_a = [0, -self.elbow_offset_y, 0, 0, 0]
         DH_alpha = [pi/2, pi/2, -pi/2, pi/2, 0]
         DH_type = [self.dq_joint_rotational] * 5
@@ -157,16 +200,16 @@ class RightArm(NaoRobot):
 
         obj = DQ_SerialManipulatorDH(DH_matrix)
 
-        # Setting base and reference:
-        tz = 1 + DQ.E * (1 / 2) * DQ([0, 0, self.shoulder_offset_z])
-        rx = cos((-pi / 2) / 2) + DQ.i * sin((-pi / 2) / 2)
-        obj.set_base_frame(tz * rx)
-        obj.set_reference_frame(tz * rx)
+        # Setting base and reference to the torso
+        tyz = 1 + DQ.E * (1/2) * DQ([0, -self.shoulder_offset_y, self.shoulder_offset_z])
+        rx = cos((-pi/2)/2) + DQ.i * sin((-pi/2)/2)
+        obj.set_base_frame(tyz * rx)
+        obj.set_reference_frame(tyz * rx)
 
-        # Setting end effector:
+        # Setting end effector
         rx = cos((-pi/2)/2) + DQ.i * sin((-pi/2)/2)
         rz = cos((-pi/2)/2) + DQ.k * sin((-pi/2)/2)
-        txz = 1 + DQ.E * (1/2) * DQ( [self.hand_offset_x + self.lower_arm_length, 0, -self.hand_offset_z])
+        txz = 1 + DQ.E * (1/2) * DQ([self.hand_offset_x, 0, -self.hand_offset_z])
         obj.set_effector(rx * rz * txz)
 
         return obj
@@ -178,27 +221,39 @@ class LeftLeg(NaoRobot):
     Joints: LHipYawPitch, LHipRoll, LHipPitch,
             LKneePitch, LAnklePitch, LAnkleRoll
     """
-    def kinematics(self):
+    def get_joints_names(self) -> [str]:
+        """
+        Return the name of the joints in the chain.
+
+        :return: a list with the name of the joints in the chain
+        """
+        return ["LHipYawPitch", "LHipRoll", "LHipPitch",
+                "LKneePitch", "LAnklePitch", "LAnkleRoll"]
+
+    def kinematics(self) -> DQ_SerialManipulatorDH:
         """
         Create the DQ_kinematics object for the chain.
 
-        :return: DQ_SerialManipulatorDH object.
+        :return: DQ_SerialManipulatorDH object
         """
+
+        # Denavit-Hartenberg parameters
         DH_theta = [pi/2, 3*pi/4, 0, 0, 0, 0]
-        DH_d = [0, 0, self.hip_offset_y, 0, 0, 0]
-        DH_a = [0, 0, -(self.hip_offset_z+self.thigh_length), -self.tibia_length, 0, 0]
+        DH_d = [0, 0, 0, 0, 0, 0]
+        DH_a = [0, 0, -self.thigh_length, -self.tibia_length, 0, 0]
         DH_alpha = [pi/2, pi/2, 0, 0, -pi/2, 0]
         DH_type = [self.dq_joint_rotational] * 6
         DH_matrix = [DH_theta, DH_d, DH_a, DH_alpha, DH_type]
 
         obj = DQ_SerialManipulatorDH(DH_matrix)
 
-        # Setting base and reference:
+        # Setting base and reference to the torso
+        tyz = 1 + DQ.E * (1/2) * DQ([0, self.hip_offset_y, -self.hip_offset_z])
         rx = cos((-pi/4)/2) + DQ.i * sin((-pi/4)/2)
-        obj.set_base_frame(rx)
-        obj.set_reference_frame(rx)
+        obj.set_base_frame(tyz * rx)
+        obj.set_reference_frame(tyz * rx)
 
-        # Setting end effector (foot):
+        # Setting end effector
         ry = cos((pi/2)/2) + DQ.j * sin((pi/2)/2)
         rz = cos(pi/2) + DQ.k * sin(pi/2)
         tz = 1 + DQ.E * (1/2) * DQ([0, 0, -self.foot_height])
@@ -213,27 +268,39 @@ class RightLeg(NaoRobot):
     Joints: RHipYawPitch, RHipRoll, RHipPitch,
             RKneePitch, RAnklePitch, RAnkleRoll
     """
-    def kinematics(self):
+    def get_joints_names(self) -> [str]:
+        """
+        Return the name of the joints in the chain.
+
+        :return: a list with the name of the joints in the chain
+        """
+        return ["RHipYawPitch", "RHipRoll", "RHipPitch",
+                "RKneePitch", "RAnklePitch", "RAnkleRoll"]
+
+    def kinematics(self) -> DQ_SerialManipulatorDH:
         """
         Create the DQ_kinematics object for the chain.
 
-        :return: DQ_SerialManipulatorDH object.
+        :return: DQ_SerialManipulatorDH object
         """
+
+        # Denavit-Hartenberg parameters
         DH_theta = [pi/2, pi/4, 0, 0, 0, 0]
-        DH_d = [0, 0, -self.hip_offset_y, 0, 0, 0]
-        DH_a = [0, 0, -(self.hip_offset_z+self.thigh_length), -self.tibia_length, 0, 0]
+        DH_d = [0, 0, 0, 0, 0, 0]
+        DH_a = [0, 0, -self.thigh_length, -self.tibia_length, 0, 0]
         DH_alpha = [pi/2, pi/2, 0, 0, -pi/2, 0]
         DH_type = [self.dq_joint_rotational] * 6
         DH_matrix = [DH_theta, DH_d, DH_a, DH_alpha, DH_type]
 
         obj = DQ_SerialManipulatorDH(DH_matrix)
 
-        # Setting base and reference:
+        # Setting base and reference to the torso
+        tyz = 1 + DQ.E * (1/2) * DQ([0, -self.hip_offset_y, -self.hip_offset_z])
         rx = cos((pi/4)/2) + DQ.i * sin((pi/4)/2)
-        obj.set_base_frame(rx)
-        obj.set_reference_frame(rx)
+        obj.set_base_frame(tyz * rx)
+        obj.set_reference_frame(tyz * rx)
 
-        # Setting end effector (foot):
+        # Setting end effector
         ry = cos((pi/2)/2) + DQ.j * sin((pi/2)/2)
         rz = cos(pi/2) + DQ.k * sin(pi/2)
         tz = 1 + DQ.E * (1/2) * DQ([0, 0, -self.foot_height])
